82ae8945c0f3be332482e3ff03c113f0030b2afc
Govind Raghu <govindkrishna@gmail.com>
2015-04-19 22:16:31 -0700

    small fix

--- Gensim.py
+++ Gensim.py
@@ -24,7 +24,7 @@ class Gensim:
         texts = [[word for word in word_tokenize(document.lower()) if word not in self.stoplist] for document in documents]
 
         frequency = defaultdict(int)
-        for text in wordlist:
+        for text in texts:
             for token in text:
                 frequency[token] += 1
         texts = [[token for token in text if frequency[token] > 1 and len(token) > 2 ] for text in texts]
