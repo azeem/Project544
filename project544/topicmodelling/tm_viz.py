d5544a56f993dd2e6eeaeddc0e9e9ff99fcd36f0
Azeem Arshad <azeem.arshadt@gmail.com>
2015-04-26 23:40:13 -0700

    tmviz accept font path change

--- project544/topicmodelling/tm_viz.py
+++ project544/topicmodelling/tm_viz.py
@@ -20,16 +20,16 @@ class TopicModelViz:
     def terms_to_wordcounts(self, terms, multiplier=1000):
         return " ".join([" ".join(int(multiplier*i[0]) * [i[1]]) for i in terms])
 
-    def genWordCloud(self, index):
-        wordcloud = WordCloud(background_color="white").generate(self.terms_to_wordcounts(self.terms[index]))
+    def genWordCloud(self, index, font_path = None):
+        wordcloud = WordCloud(background_color="white", font_path=font_path).generate(self.terms_to_wordcounts(self.terms[index]))
         plt.imshow(wordcloud)
         plt.axis("off")
         plt.savefig(os.path.join(config.VISUALIZATION_FOLDER,"wordcloud_"+str(index)))
         plt.close()
 
-    def genWordClouds(self):
+    def genWordClouds(self, font_path = None):
         for i in range(config.NUM_TOPICS_LDA):
-            self.genWordCloud(i)
+            self.genWordCloud(i, font_path = font_path)
 
     def genTopicProportion(self, question):
         topic_prop = self.topicmodel.getDocumentFeatures(question)
@@ -44,5 +44,9 @@ class TopicModelViz:
         plt.close()
 
 if __name__ == "__main__":
+    import sys
+    font_path = None
+    if len(sys.argv) == 2:
+        font_path = sys.argv[1]
     topicmodelviz = TopicModelViz()
-    topicmodelviz.genWordClouds()
+    topicmodelviz.genWordClouds(font_path = font_path)
